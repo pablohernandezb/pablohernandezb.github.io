df9281ab9ad4769e027da93f63cae178c33c83e1
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-01-15 17:07:24 -0500

    Changed App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Pablo Hernandez Borges");
+    expect(document.title).toBe('Pablo Hernandez Borges');
   });
 
   it('can navigate to /about', async () => {
