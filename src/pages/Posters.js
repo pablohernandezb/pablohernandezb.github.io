d1faa9a2eb0964a62e9d2f079486871dcdbffe02
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-01-17 20:58:23 -0500

    Adding text to the posters section

--- src/pages/Posters.js
+++ src/pages/Posters.js
@@ -17,6 +17,9 @@ const Posters = () => (
           <p>A selection of posters presented at research conferences</p>
         </div>
       </header>
+      <p>
+        Most of them use the posters use the LaTeX font, you can find it <a href="https://www.gust.org.pl/projects/e-foundry/latin-modern">here</a>. The ones with the big image in the middle are inspired by the <a href="https://osf.io/ef53g/">Better Poster Design</a> framework.
+      </p>
       {data.map((poster) => (
         <Sheet data={poster} key={poster.title} />
       ))}
