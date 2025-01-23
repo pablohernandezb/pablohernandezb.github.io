e41a50ff7e691023457b7ff60539552a71e1eaf3
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-01-23 17:52:46 -0500

    Arranging the order of the projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,20 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'The Long Trace of Oil Money: A Social Network Analysis of corruption during the Bolivarian Revolution in Venezuela',
-    link: 'https://github.com/pablohernandezb/the-long-trace-of-oil-money',
-    image: '/images/projects/project_long_trace.png',
-    date: '2021-05-01',
-    desc:
-      'Using the Chavismo.inc dataset gathered by a team lead by Transparencia Venezuela.',
-  },
-  {
     title: 'The Crisis of Divided Government: The Effect of Democratic Backsliding on Legislative Oversight in Venezuela',
     link: 'https://github.com/pablohernandezb/the-crisis-of-divided-government',
     image: '/images/projects/project_crisis_divided_govt.png',
     date: '2023-04-01',
     desc:
       'Using the V-Dem Project dataset, and Synthetic Control Method (SMC).',
+  },
+  {
+    title: 'The Long Trace of Oil Money: A Social Network Analysis of corruption during the Bolivarian Revolution in Venezuela',
+    link: 'https://github.com/pablohernandezb/the-long-trace-of-oil-money',
+    image: '/images/projects/project_long_trace.png',
+    date: '2021-05-01',
+    desc:
+      'Using the Chavismo.inc dataset gathered by a team lead by Transparencia Venezuela.',
   },
 ];
 
