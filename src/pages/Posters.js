5db75f3ba0c167b43dc2b9e539d58d1d8705d33a
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-02-03 00:50:11 -0500

    Fixing posters.js errors

--- src/pages/Posters.js
+++ src/pages/Posters.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import FsLightbox from "fslightbox-react";
 
 import Main from '../layouts/Main';
 
