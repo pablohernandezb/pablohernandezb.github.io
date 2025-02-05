93445c7e8d7104a7314d499cff3bc82e2f7ab479
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-02-05 14:15:16 -0500

    Trying again

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ const config = {
     '^.+\\.(css|less|scss)$': 'babel-jest',
     '^.+\\.md$': 'markdown-to-jsx',
   },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
   transformIgnorePatterns: ['/node_modules/(?!jest-runtime|lightbox\\.js-react)'],
 };
 
