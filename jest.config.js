3c306ddae94562e30dfd64d585a006eb94f6cfa6
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-02-05 16:56:04 -0500

    Adding some configurations to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,16 @@ const config = {
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+
   },
-  transformIgnorePatterns: ['/node_modules/(?!@react-hook|jest-runtime|lightbox\\.js-react)'],
+  transformIgnorePatterns: ['/node_modules/(?!@react-hook|lightbox\\.js-react)*\\.(js|tsx)$'],
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"]
 };
 
 module.exports = config;
