b4e0c1965bf9d09c568426177598c04c313583f5
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-02-04 23:13:42 -0500

    Updating jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ const config = {
     '^.+\\.(css|less|scss)$': 'babel-jest',
     '^.+\\.md$': 'markdown-to-jsx',
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!axios|lightbox\\.js-react).+\\.js$',
+  ],
 };
 
 module.exports = config;
